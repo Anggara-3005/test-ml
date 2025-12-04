1b2d43273f7930b65f6d5e06cfdc861512d2e2db
Anggara-3005 <158810538+Anggara-3005@users.noreply.github.com>
2025-12-05 05:24:50 +0700

    Refactor training script to use RandomForestClassifier
    Updated the model to use RandomForestClassifier and changed the dataset to 'winequality-red.csv'. Added accuracy logging to a file.

--- train.py
+++ train.py
@@ -1,26 +1,26 @@
 import pandas as pd
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassfier
 from sklearn.metrics import accuracy_score
 import joblib
 
-# Load dataset
-iris = load_iris()
-X, y = iris.data, iris.target
+df = pd.read_csv('winequality-red.csv')
 
-# Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X = df.drop('quality', axis=1)
+y = df['quality']
 
-# Train model
-model = LogisticRegression(max_iter=200)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(n_estimators=10, random_state=42)
 model.fit(X_train, y_train)
 
-# Evaluate
-y_pred = model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
+predictions = model.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
 
-print(f"Accuracy: {acc:.2f}")
+print(f"Accuracy: {accuracy}")
 
+with open("metrics.txt", "w") as f:
+  f.write(f"Accuracy: {accuracy}\n")
+  
 # Save model
 joblib.dump(model, "model.pkl")
